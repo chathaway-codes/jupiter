9f14f40b1981dbaccf2487f9845e125635d19d92
Charles H <chathaway@logrit.com>
2013-12-20 12:04:34 -0500

    More small changes

--- jupiter/settings.py
+++ jupiter/settings.py
@@ -62,15 +62,11 @@ MEDIA_ROOT = PROJECT_ROOT + '/media/'
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = '/media/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = PROJECT_ROOT + '/collected_static/'
-
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
 
 # Additional locations of static files
 STATICFILES_DIRS = (
