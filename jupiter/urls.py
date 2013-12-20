18d3b8ab49a7c7d8d916152d90b93e47d8279ce8
Charles H <chathaway@logrit.com>
2013-12-19 23:10:50 -0500

    IT WASN'T ME!

--- jupiter/urls.py
+++ jupiter/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
-from registration.backends.simple.views import RegistrationView
 
 from jupiter.views import ReadingCreate, PhysicalActivityCreate
 from jupiter.views import ActivityCreate
